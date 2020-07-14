371911ae7dff6c390b26553d371a4114bbaaa67f
OctopusLian <zoctopus@qq.com>
2020-07-14 17:15:51 +0800

    子命令的使用

--- main.go
+++ main.go
@@ -5,17 +5,35 @@ import (
 	"log"
 )
 
+var name string
+
 func main() {
-	var name string
-	flag.StringVar(&name, "name", "Go语言编程之旅", "帮助信息")
-	flag.StringVar(&name, "n", "Go语言编程之旅", "帮助信息")
 	flag.Parse()
+	goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+	goCmd.StringVar(&name, "name", "Go语言", "帮助信息")
+	phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+	phpCmd.StringVar(&name, "n", "PHP语言", "帮助信息")
+
+	args := flag.Args()
+	switch args[0] {
+	case "go":
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		_ = goCmd.Parse(args[1:])
+	}
 
 	log.Printf("name: %s", name)
 
 }
 
 /*
-# go run main.go -name=neozhang -n=念念
-2020/07/14 16:13:55 name: 念念
+# go run main.go go -n=neo
+flag provided but not defined: -n
+Usage of go:
+  -name string
+    	帮助信息 (default "Go语言")
+exit status 2
+# go run main.go go -name=neo
+2020/07/14 17:15:01 name: neo
+
 */
